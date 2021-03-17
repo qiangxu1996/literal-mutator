1c808b30294037b9b68396b2d431efdb6aa96c99
Qiang Xu <qiangxu1996@gmail.com>
2021-03-16 20:48:12 -0400

    also support csv output

--- src/main/scripts/bookmark-extract-path.py
+++ src/main/scripts/bookmark-extract-path.py
@@ -1,13 +1,26 @@
+import argparse
+import csv
 import json
-import sys
 
 if __name__ == '__main__':
-    with open(sys.argv[1], encoding='utf-8') as f:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bookmark')
+    parser.add_argument('--csv')
+    parser.add_argument('--txt')
+    args = parser.parse_args()
+
+    with open(args.bookmark, encoding='utf-8') as f:
         bookmarks = json.load(f)
-    paths = []
+    if args.csv:
+        csvfile = open(args.csv, 'w', newline='')
+        csvwriter = csv.writer(csvfile)
+    if args.txt:
+        pathfile = open(args.txt, 'w', encoding='utf-8')
+
     for b in bookmarks:
         for bb in b['bookmarks']:
-            path = bb['label'].rsplit('@', 1)[-1]
-            paths.append(path + '\n')
-    with open(sys.argv[2], 'w', encoding='utf-8') as f:
-        f.writelines(paths)
+            line, path = bb['label'].rsplit('@', 1)
+            if args.csv:
+                csvwriter.writerow((path, line))
+            if args.txt:
+                pathfile.write(path + '\n')
